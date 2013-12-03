b0185c4bcf6bb21a53251fd071790f62dfcb3724
David Fisher <tibbon@gmail.com>
2013-12-03 16:06:39 -0500

    Can add/remove items from cart now

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,14 @@
 class LineItemsController < ApplicationController
+	def index
+		@line_items = LineItem.all
+	end
 	def create
 		LineItem.create(user_id: params[:user_id], product_id: params[:product_id])
+		redirect_to :cart
+	end
+
+	def delete
+		LineItem.find(params[:line_item_id]).destroy
 		redirect_to :cart
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ ProductFront::Application.routes.draw do
   root 'products#index'
   resources :products, only: [:index, :show]
   post '/products/add_to_cart', to: 'line_items#create', as: 'new_line_item'
+  get '/cart', to: 'line_items#index', as: 'cart'
+  delete '/cart', to: 'line_items#delete', as: 'remove_item_from_cart'
 end
