e7c34c84360ac52682ae18cbe2eb30ca6b2ddd79
David Fisher <tibbon@gmail.com>
2013-12-03 14:59:25 -0500

    ran a generator for Products

---
+++ db/migrate/20131203195909_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.text :name
+      t.text :description
+      t.decimal :price
+      t.text :images
+
+      t.timestamps
+    end
+  end
+end
